ea86c7dcd884afbc0b817a62a7c520bfe8d8a3e3
Petr <fateyevpetr@gmail.com>
2023-08-26 13:22:13 +0700

    Initial commit

---
+++ showcase-of-goods/src/main/java/fateyev/ru/showcaseOfProducts/ShowcaseOfGoodsApplication.java
@@ -0,0 +1,13 @@
+package fateyev.ru.showcaseOfProducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowcaseOfGoodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowcaseOfGoodsApplication.class, args);
+	}
+
+}
---
+++ showcase-of-goods/src/test/java/fateyev/ru/showcaseOfProducts/ShowcaseOfGoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package fateyev.ru.showcaseOfProducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowcaseOfGoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
