ab29b74226200f96f6a2632ade7018a5fd54c70d
Petr <fateyevpetr@gmail.com>
2023-08-26 22:21:50 +0700

    change repository: extends JpaSpecificationExecutor<>, for method findAll(Specification sp)

--- showcase-of-goods/src/main/java/fateyev/ru/showcaseOfProducts/repositories/ProductRepository.java
+++ showcase-of-goods/src/main/java/fateyev/ru/showcaseOfProducts/repositories/ProductRepository.java
@@ -2,12 +2,13 @@ package fateyev.ru.showcaseOfProducts.repositories;
 
 import fateyev.ru.showcaseOfProducts.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, UUID> {
+public interface ProductRepository extends JpaRepository<Product, UUID>, JpaSpecificationExecutor<Product> {
 
-    List<Product> findByShowcaseId(UUID id);
+    List<Product> findByShowcaseShowcaseId(UUID id);
 }
--- showcase-of-goods/src/main/java/fateyev/ru/showcaseOfProducts/repositories/ShowcaseRepository.java
+++ showcase-of-goods/src/main/java/fateyev/ru/showcaseOfProducts/repositories/ShowcaseRepository.java
@@ -2,9 +2,11 @@ package fateyev.ru.showcaseOfProducts.repositories;
 
 import fateyev.ru.showcaseOfProducts.models.Showcase;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface ShowcaseRepository extends JpaRepository<Showcase, UUID> {
+public interface ShowcaseRepository extends JpaRepository<Showcase, UUID>, JpaSpecificationExecutor<Showcase> {
+
 }
