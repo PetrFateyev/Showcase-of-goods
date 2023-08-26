45040284217236aab3a480582a0c5164d6919fd3
Petr <fateyevpetr@gmail.com>
2023-08-26 22:23:23 +0700

    change method findAll

--- showcase-of-goods/src/main/java/fateyev/ru/showcaseOfProducts/services/ShowcaseService.java
+++ showcase-of-goods/src/main/java/fateyev/ru/showcaseOfProducts/services/ShowcaseService.java
@@ -4,9 +4,11 @@ import fateyev.ru.showcaseOfProducts.models.Showcase;
 import fateyev.ru.showcaseOfProducts.repositories.ShowcaseRepository;
 import fateyev.ru.showcaseOfProducts.utils.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -25,10 +27,9 @@ public class ShowcaseService {
         showcaseRepository.save(showcase);
     }
 
-    public List<Showcase> findAll(){
-        return showcaseRepository.findAll();
+    public Page<Showcase> findAll(Specification<Showcase> specification, Pageable pageable){
+        return showcaseRepository.findAll(specification, pageable);
     }
-
     @Transactional
     public void update(UUID id, Showcase updatedShowcase) {
         if (!showcaseRepository.existsById(id)){
