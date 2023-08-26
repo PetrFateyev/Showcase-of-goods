09f64200bd153902727f35eb625a1ac61327ff9a
Petr <fateyevpetr@gmail.com>
2023-08-26 14:54:45 +0700

    add method findByShowcaseId(UUID id), then get all  products of showcase

--- showcase-of-goods/src/main/java/fateyev/ru/showcaseOfProducts/repositories/ProductRepository.java
+++ showcase-of-goods/src/main/java/fateyev/ru/showcaseOfProducts/repositories/ProductRepository.java
@@ -3,8 +3,11 @@ package fateyev.ru.showcaseOfProducts.repositories;
 import fateyev.ru.showcaseOfProducts.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+
+    List<Product> findByShowcaseId(UUID id);
 }
