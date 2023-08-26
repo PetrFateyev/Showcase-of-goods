f8bc9073aeb7930a119933876cf1d4ff938518db
Petr <fateyevpetr@gmail.com>
2023-08-27 00:31:32 +0700

    delete method: findByShowcaseShowcaseId(UUID id)

--- showcase-of-goods/src/main/java/fateyev/ru/showcaseOfProducts/repositories/ProductRepository.java
+++ showcase-of-goods/src/main/java/fateyev/ru/showcaseOfProducts/repositories/ProductRepository.java
@@ -4,11 +4,9 @@ import fateyev.ru.showcaseOfProducts.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID>, JpaSpecificationExecutor<Product> {
 
-    List<Product> findByShowcaseShowcaseId(UUID id);
 }
