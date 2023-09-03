8cbf5f580b407b8b61845e61ced1739ca71183c7
Petr <fateyevpetr@gmail.com>
2023-09-03 23:17:17 +0700

    add findAllByShowcaseShowcaseId(UUID showcaseId)

--- showcase-of-goods/src/main/java/fateyev/ru/showcaseOfProducts/repositories/ProductRepository.java
+++ showcase-of-goods/src/main/java/fateyev/ru/showcaseOfProducts/repositories/ProductRepository.java
@@ -4,9 +4,10 @@ import fateyev.ru.showcaseOfProducts.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID>, JpaSpecificationExecutor<Product> {
-
+    List<Product> findAllByShowcaseShowcaseId(UUID showcaseId);
 }
